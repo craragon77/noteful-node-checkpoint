e8dfc1e0929c3bb2d96dcba850431d1a4096fb87
Chris Aragon <aragonchristopherray@gmail.com>
2020-05-25 10:19:17 -0400

    first commit

---
+++ seed/seed.folder.sql
@@ -0,0 +1,5 @@
+INSERT INTO folder (title, note_id)
+VALUES
+('first folder', 1),
+('second folder', 2),
+('third folder', 3);
---
+++ seed/seed.note.sql
@@ -0,0 +1,7 @@
+INSERT INTO note (title, content)
+VALUES
+('fist note!', 'lorem imsup'),
+('second note!', 'lorem imsup'),
+('third note!', 'lorem imsup'),
+('fourth note!', 'lorem imsup'),
+('fifth note!', 'lorem imsup');
